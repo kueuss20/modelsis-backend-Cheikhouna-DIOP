03e8185e9c7c4380ad1443b7920b69275194a424
cheikhouna <kueuss20@gmail.com>
2024-01-04 15:31:18 +0000

    first commit

---
+++ src/main/java/modelsis/cheikhounadiop/ModelsischeikhounadiopApplication.java
@@ -0,0 +1,13 @@
+package modelsis.cheikhounadiop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelsischeikhounadiopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelsischeikhounadiopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/modelsis/cheikhounadiop/ModelsischeikhounadiopApplicationTests.java
@@ -0,0 +1,13 @@
+package modelsis.cheikhounadiop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelsischeikhounadiopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
